61582f82bbca97bc955d6d76646a28b8606227f0
Ignacio Cabre Barrera <cabrei@gsicommece.com>
2014-05-16 08:12:32 +0200

    added list of ops

--- src/x0/core.cljs
+++ src/x0/core.cljs
@@ -1,13 +1,29 @@
 (ns x0.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
+
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def max-ops 5)
+(def ops (repeatedly max-ops #(vector (rand-int 9) (rand-int 9))))
+
+(def app-state (atom {:ops ops}))
+
+
+#_(om/root
+  (fn [app owner]
+    (dom/h2 nil "Hello World"))
+  app-state
+  {:target (. js/document (getElementById "app"))}) ;; <-- "app0" to "app1"
+
+
 
 (om/root
   (fn [app owner]
-    (dom/h1 nil (:text app)))
+    (apply dom/ul nil
+      (map (fn [[a b]] (dom/li nil (str a " * " b "=" (* a b)))) (:ops app))))
   app-state
   {:target (. js/document (getElementById "app"))})
