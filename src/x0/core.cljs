9393220457abdaa1ee476354c3cc3a961fc2b2b9
Ignacio Cabre Barrera <cabrei@gsicommece.com>
2014-05-16 08:31:42 +0200

    added input field and button with no logic

--- src/x0/core.cljs
+++ src/x0/core.cljs
@@ -7,23 +7,22 @@
 
 (enable-console-print!)
 
+(def op-sign "*")
 (def max-ops 5)
 (def ops (repeatedly max-ops #(vector (rand-int 9) (rand-int 9))))
 
 (def app-state (atom {:ops ops}))
 
 
-#_(om/root
-  (fn [app owner]
-    (dom/h2 nil "Hello World"))
-  app-state
-  {:target (. js/document (getElementById "app"))}) ;; <-- "app0" to "app1"
-
-
+(defn ops-view [app owner]
+  (apply dom/ul nil
+         (map (fn [[a b]] (dom/div nil 
+                                   (str a op-sign b "= ")
+                                   (dom/input #js {:type "text" :value "test1"})
+                                   (dom/button nil "submit")))
+              (:ops app))))
 
 (om/root
-  (fn [app owner]
-    (apply dom/ul nil
-      (map (fn [[a b]] (dom/li nil (str a " * " b "=" (* a b)))) (:ops app))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+ ops-view
+ app-state
+ {:target (. js/document (getElementById "app"))})
