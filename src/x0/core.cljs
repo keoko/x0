d0e0207c9dcabd5b2933894341bf2d5ddd42e35d
Ignacio Cabre Barrera <cabrei@gsicommece.com>
2014-05-17 20:57:30 +0200

    added sablono library, implemented scoring

--- src/x0/core.cljs
+++ src/x0/core.cljs
@@ -2,25 +2,60 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [sablono.core :as html :refer-macros [html]]))
 
 
 (enable-console-print!)
 
 (def op-sign "*")
 (def max-ops 5)
-(def ops (repeatedly max-ops #(vector (rand-int 9) (rand-int 9))))
+(def ops [[1 2] [3 4] [5 6] [7 8] [1 1]])
 
-(def app-state (atom {:ops ops}))
+(def app-state (atom {:score 0 :ops ops :pos 0}))
 
+(defn handle-change [e owner {:keys [text]}]
+  (om/set-state! owner :val (.. e -target -value)))
+
+
+(defn check-op [e app x y pos owner]
+  (let [val (js/parseInt (.. e -target -value))]
+    (if (= val (* x y))
+      (do 
+        (.alert js/window "Molt bé")
+        (om/transact! app :pos #(inc %))
+        (om/transact! app :score #(inc %))
+        (om/set-state! owner :val ""))
+      (do
+        (.alert js/window (str "Noo .. no passa res, prova amb la següent"))
+        (om/transact! app :pos #(inc %))
+        (om/set-state! owner :val "")))))
+
+
+(defn score-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (html [:div "punts:" (:score app) "/" max-ops]))))
 
 (defn ops-view [app owner]
-  (apply dom/ul nil
-         (map (fn [[a b]] (dom/div nil 
-                                   (str a op-sign b "= ")
-                                   (dom/input #js {:type "text" :value "test1"})
-                                   (dom/button nil "submit")))
-              (:ops app))))
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:val ""})
+    om/IRenderState
+    (render-state [_ state]
+      (let [pos (:pos app)
+            [x y] (nth (:ops app) pos)
+            operator '*]
+        (html 
+         [:div x "*" y "="
+          (om/build score-view app)
+          (html/text-field {:value (:val state)
+                            :on-change #(handle-change % owner state)
+                            :on-key-press  #(when (== (.-keyCode %) 13) 
+                                              (check-op % app x y pos owner))} 
+                           :foo)])))))
 
 (om/root
  ops-view
